a18c6f482b3f9802765d7e1cc8d423907b0efe14
FilipHarald <filip.harald@gmail.com>
2021-12-22 20:12:56 +0100

    first working mock

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,19 @@
-from brownie import FundMe
+from brownie import FundMe, MockV3Aggregator, network, config
 from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account})
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+    else:
+        print(f"The active netowrk is {network.show_active()}")
+        print("Deploying mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(18, 2000000000000000000, {"from": account})
+        print("Mocks deployed!")
+        price_feed_address = mock_aggregator.address
+
+    fund_me = FundMe.deploy(price_feed_address, {"from": account}, publish_source=config["networks"][network.show_active()].get("verify"))
     print(f"Contract deployed to {fund_me.address}")
 
 
