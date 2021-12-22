3945e0c4a3b3dc80391bb1b6b39a39f37f5e765a
FilipHarald <filip.harald@gmail.com>
2021-12-22 22:37:27 +0100

    tests working and forked mainnet

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,20 +1,17 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
-
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account, deploy_mocks
 
 def deploy_fund_me():
     account = get_account()
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
     else:
-        print(f"The active netowrk is {network.show_active()}")
-        print("Deploying mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(18, 2000000000000000000, {"from": account})
-        print("Mocks deployed!")
-        price_feed_address = mock_aggregator.address
+        deploy_mocks(account)
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(price_feed_address, {"from": account}, publish_source=config["networks"][network.show_active()].get("verify"))
     print(f"Contract deployed to {fund_me.address}")
+    return fund_me
 
 
 def main():
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,7 +1,23 @@
-from brownie import network, accounts, config
+from brownie import MockV3Aggregator, network, accounts, config
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 
 def get_account():
-    if(network.show_active() == "development"):
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or
+        network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+def deploy_mocks(account):
+    print(f"The active netowrk is {network.show_active()}")
+    print("Deploying mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": account})
+        print("Mocks deployed!")
+    price_feed_address = MockV3Aggregator[-1].address
